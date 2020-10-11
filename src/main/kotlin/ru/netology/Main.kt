ca202b7a2b9f471f1ab270e20fae623cb1e236bc
Mashnin Egor <y0g@mail.ru>
2020-10-11 16:21:57 +1000

    functions minutesAgoToText and hoursAgoToText added

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -10,28 +10,32 @@ fun main() {
 fun agoToText(secondsAgo: Int): String? {
     val minutesAgo = secondsAgo / 60
     val hoursAgo = minutesAgo / 60
-    if (secondsAgo >= 0) {
+    return if (secondsAgo >= 0) {
         when (secondsAgo) {
-            in 0..60 -> return "только что"
-            in 61..60 * 60 -> return when {
-                minutesAgo % 10 == 1 && minutesAgo % 100 != 11 -> "$minutesAgo минуту назад"
-                (minutesAgo % 10 == 2 && minutesAgo % 100 != 12) ||
-                        (minutesAgo % 10 == 3 && minutesAgo % 100 != 13) ||
-                        (minutesAgo % 10 == 4 && minutesAgo % 100 != 14) -> "$minutesAgo минуты назад"
-                else -> "$minutesAgo минут назад"
-            }
-            in 60 * 60 + 1..24 * 60 * 60 -> return when {
-                hoursAgo % 10 == 1 && hoursAgo % 100 != 11 -> "$hoursAgo час назад"
-                (hoursAgo % 10 == 2 && hoursAgo % 100 != 12) ||
-                        (hoursAgo % 10 == 3 && hoursAgo % 100 != 13) ||
-                        (hoursAgo % 10 == 4 && hoursAgo % 100 != 14) -> "$hoursAgo часа назад"
-                else -> "$hoursAgo часов назад"
-            }
-            in 24 * 60 * 60 + 1..2 * 24 * 60 * 60 -> return "сегодня"
-            in 2 * 24 * 60 * 60 + 1..3 * 24 * 60 * 60 -> return "вчера"
-            else -> return "давно"
+            in 0..60 -> "только что"
+            in 61..60 * 60 -> minutesAgoToText(minutesAgo)
+            in 60 * 60 + 1..24 * 60 * 60 -> hoursAgoToText(hoursAgo)
+            in 24 * 60 * 60 + 1..2 * 24 * 60 * 60 -> "сегодня"
+            in 2 * 24 * 60 * 60 + 1..3 * 24 * 60 * 60 -> "вчера"
+            else -> "давно"
         }
-    } else return null
+    } else null
+}
+
+fun minutesAgoToText(minutesAgo : Int) = when {
+    minutesAgo % 10 == 1 && minutesAgo % 100 != 11 -> "$minutesAgo минуту назад"
+    (minutesAgo % 10 == 2 && minutesAgo % 100 != 12) ||
+            (minutesAgo % 10 == 3 && minutesAgo % 100 != 13) ||
+            (minutesAgo % 10 == 4 && minutesAgo % 100 != 14) -> "$minutesAgo минуты назад"
+    else -> "$minutesAgo минут назад"
+}
+
+fun hoursAgoToText(hoursAgo : Int) = when {
+    hoursAgo % 10 == 1 && hoursAgo % 100 != 11 -> "$hoursAgo час назад"
+    (hoursAgo % 10 == 2 && hoursAgo % 100 != 12) ||
+            (hoursAgo % 10 == 3 && hoursAgo % 100 != 13) ||
+            (hoursAgo % 10 == 4 && hoursAgo % 100 != 14) -> "$hoursAgo часа назад"
+    else -> "$hoursAgo часов назад"
 }
 
 fun printAgo(from: Int, till: Int, step: Int = 1) {
